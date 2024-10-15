78f6183274915f679d6e286a38fdd53beb988c65
Elliott Wagner <elliottjwag@hotmail.com>
2024-10-14 19:36:12 -0600

    EX2

---
+++ CH01/EX2/EX2.js
@@ -0,0 +1,16 @@
+//Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert (must output “John”).
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin)
+
+//Create a variable with the name of our planet.
+//How would you name such a variable?
+let outPlanet = "earth";
+//Create a variable to store the name of a current visitor to a website.
+//How would you name that variable?
+let currentVisitor = "John";
