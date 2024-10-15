2f9728a042333898007a5f49e58d19cd4665eedc
wagner3956 <elliottjwag@hotmail.com>
2024-10-15 15:09:47 -0600

    EX7  Done

--- CH01/EX7/loops.js
+++ CH01/EX7/loops.js
@@ -1,7 +1,7 @@
 //Use the for loop to output even numbers from 2 to 10.
-// for(let i = 2; i <= 10; i++){
-//     alert(i)
-// }
+for(let i = 2; i <= 10; i++){
+    alert(i)
+}
 
 //Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).
 // for (let i = 0; i < 3; i++) {
@@ -19,15 +19,26 @@ while(i < 3){
 //Here we can assume that the visitor only inputs numbers.
 //There’s no need to implement a special handling for a non-numeric input in this task.
 
+let number = prompt("Enter a number more than 100", '');
+while(number < 100){
+    number = prompt("Enter a number more than 100", '');
+}
 
 //An integer number greater than 1 is called a prime if it cannot be divided without a remainder by anything except 1 and itself.
-//
 // In other words, n > 1 is a prime if it can’t be evenly divided by anything except 1 and n.
-//
 // For example, 5 is a prime, because it cannot be divided without a remainder by 2, 3 and 4.
-//
 // Write the code which outputs prime numbers in the interval from 2 to n.
-//
 // For n = 10 the result will be 2,3,5,7.
-//
 // P.S. The code should work for any n, not be hard-tuned for any fixed value.
+
+let n = prompt("What would you like to do?", '');
+
+nextPrime:
+    for (let i = 2; i <= n; i++) { // for each i...
+
+        for (let j = 2; j < i; j++) { // look for a divisor..
+            if (i % j == 0) continue nextPrime; // not a prime, go next i
+        }
+
+        alert( i ); // a prime
+    }
