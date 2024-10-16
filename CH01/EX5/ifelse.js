69e6c051efe645116476cf8969a998960ccd8e3a
Elliott Wagner <elliottjwag@hotmail.com>
2024-10-15 22:49:54 -0600

    EX9

--- CH01/EX5/ifelse.js
+++ CH01/EX5/ifelse.js
@@ -2,16 +2,19 @@
 if ("0") {
     alert( 'Hello' );
 }
+
 //Using the if..else construct, write the code which asks:
 //‘What is the “official” name of JavaScript?’
 // If the visitor enters “ECMAScript”, then output “Right!”,
 //otherwise – output: “You don’t know? ECMAScript!”
+
 let answer = prompt('What is the "official" name of JavaScript?', 'ECMAScript');
 if(answer === 'ECMAScript'){
     alert('Right!')
 }else{
     alert('You don’t know? ECMAScript!')
 }
+
 
 //Using if..else, write the code which gets a number via prompt and then shows in alert:
 // 1, if the value is greater than zero,
@@ -22,6 +25,7 @@ let number = prompt("Enter number:", '');
 let returnNumber = number > 0 ? 1 : number < 0 ? -1 : number == 0 ? 0 : number;
 alert(returnNumber);
 
+
 //let result;
 //
 // if (a + b < 4) {
@@ -31,6 +35,7 @@ alert(returnNumber);
 // }
 
 let result = a + b < 4 ? 'Below' : 'Over';
+
 
 // Rewrite if..else using multiple ternary operators '?'.
 //
---
+++ CH01/EX9/functions.js
@@ -0,0 +1,52 @@
+//The following function returns true if the parameter age is greater than 18.
+//
+// Otherwise it asks for a confirmation and returns its result.
+//
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm('Did parents allow you?');
+//   }
+// }
+// Rewrite it, to perform the same, but without if, in a single line.
+//
+// Make two variants of checkAge:
+//
+// Using a question mark operator ?
+// Using OR ||
+function checkAge(age){
+    return (age > 18) || confirm('Did parents allow you?');
+}
+
+//Write a function min(a,b) which returns the least of two numbers a and b.
+// For instance:
+// min(2, 5) == 2
+// min(3, -1) == -1
+// min(1, 1) == 1
+function findMin(a,b){
+    return a>b ? b : a;
+}
+
+//Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
+//
+// pow(3, 2) = 3 * 3 = 9
+// pow(3, 3) = 3 * 3 * 3 = 27
+// pow(1, 100) = 1 * 1 * ...* 1 = 1
+// Create a web-page that prompts for x and n, and then shows the result of pow(x,n).
+
+function pow(x,n){
+    let answer = 0;
+    for(let i = 1; i < n; i++){
+        answer *= x;
+    }
+    return answer;
+}
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+
+if (n < 1) {
+    alert(`Power ${n} is not supported, use a positive integer`);
+} else {
+    alert( pow(x, n) );
+}
