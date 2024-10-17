b6b00eb52b10e297cdb116d3dc79f37105ada16e
wagner3956 <elliottjwag@hotmail.com>
2024-10-17 09:42:04 -0600

    syntax

---
+++ CH02/EX1/syntax.js
@@ -0,0 +1,38 @@
+//What’s wrong with the code style below?
+//
+// function pow(x,n)
+// {
+//   let result=1;
+//   for(let i=0;i<n;i++) {result*=x;}
+//   return result;
+// }
+//
+// let x=prompt("x?",''), n=prompt("n?",'')
+// if (n<=0)
+// {
+//   alert(`Power ${n} is not supported, please enter an integer number greater than zero`);
+// }
+// else
+// {
+//   alert(pow(x,n))
+// }
+
+function pow(x, n){
+    let result = 1;
+
+    for(let i = 0; i < n; i++){
+        result *= x;
+    }
+
+    return result;
+}
+
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+
+if(n <= 0){
+    alert(`Power ${n} is not supported,
+     use an integer that has a value that is greater than 0`);
+} else {
+    alert( pow(x, n) );
+}
