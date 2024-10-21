0d2d81e6985eb69ed817c2939a4e5c54051029ee
wagner3956 <elliottjwag@hotmail.com>
2024-10-21 09:36:50 -0600

    calculator

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,2 +1,18 @@
 //https://www.youtube.com/watch?v=I5kj-YsmWjM
 //Bro Code: Build this JS calculator in 15 minutes! 🖩
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+function clearDisplay(){
+    display.value = "";
+}
+function calculate(){
+    try {
+        display.value = eval(display.value).toFixed(4);
+    }catch (error){
+        display.value = "Error";
+    }
+}
