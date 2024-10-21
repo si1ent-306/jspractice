a0690c657b6404bb8ccf1da696eaeb72b37519a2
Elliott Wagner <elliottjwag@hotmail.com>
2024-10-20 23:03:27 -0600

    Weather app and Calculator

---
+++ Calculator/index.js
@@ -0,0 +1,2 @@
+//https://www.youtube.com/watch?v=I5kj-YsmWjM
+//Bro Code: Build this JS calculator in 15 minutes! 🖩
---
+++ WeatherApp/index.js
@@ -0,0 +1,2 @@
+//https://www.youtube.com/watch?v=VaDUGPMjzOM
+//Bro Code: Build a JavaScript WEATHER APP in 30+ minutes! ☀️
