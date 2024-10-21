3ad97dbf74239dbcc011a411f2d1fd6ff953ea0d
Elliott Wagner <elliottjwag@hotmail.com>
2024-10-20 23:01:24 -0600

    Pokemon

--- APIPractice/Pokemon/index.js
+++ APIPractice/Pokemon/index.js
@@ -1,10 +1,12 @@
+//https://www.youtube.com/watch?v=37vxWr0WgQk
+//Bro Code: How to FETCH data from an API using JavaScript ↩️
+
 /*   fetch = Function used for making HTTP requests to fetch resources.*/
 /*   (JSON style data, images, files)*/
 /*    Simplifies asynchronous data fetching in JavaScript and*/
 /*    used for interacting with APIs to retrieve and send*/
 /*    data asynchronously over the web.*/
 /*    fetch(url, {options})*/
-
 
 // fetch("https://pokeapi.co/api/v2/pokemon/tepig")
 //     .then(response => {
